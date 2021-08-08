81d4ee72877c2cee1d579205b1abd060421ad72b
Justin Byrne <theskaterjesus@gmail.com>
2021-08-08 19:44:34 +0100

    Added fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          'very-pale': 'hsl(225, 100%, 98%)',
+          pale: 'hsl(225, 100%, 94%)',
+          bright: 'hsl(245, 75%, 52%)',
+          desaturated: 'hsl(224, 23%, 55%)',
+          dark: 'hsl(223, 47%, 23%)',
+        }
+      },
+      fontFamily: {
+        'red-hat-display': 'Red Hat Display, sans-serif',
+      },
+    },
   },
   variants: {
     extend: {},
